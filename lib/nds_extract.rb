95edea974f97d6a274df4d060814276c21962a56
Tisdale Fry <tisdalefry@gmail.com>
2020-02-26 23:42:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -23,12 +23,11 @@ def directors_totals(nds)
   total = 0
   name = nds[row_idx][:name]
   while row_idx < nds.length do
-    total = gross_for_director(directors_database)
-    row_idx += 1
+    total = gross_for_director(nds)
   end
   result[name] = total
   total = 0
-  
+  row_idx += 1
 end
 p result
 
